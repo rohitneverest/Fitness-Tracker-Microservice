68adcfdf97e0d38bb56bffd9beafd82eefb81729
Rohit <88888rohitsharma@gmail.com>
2026-07-31 18:03:40 +0530

    debugging mongo issue1

--- waterservice/src/main/java/com/fitness/waterservice/WaterserviceApplication.java
+++ waterservice/src/main/java/com/fitness/waterservice/WaterserviceApplication.java
@@ -4,30 +4,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.mongodb.autoconfigure.MongoProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WaterserviceApplication {
 
-	@Value("${spring.data.mongodb.uri:NOT_FOUND}")
-	private String mongoUri;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaterserviceApplication.class, args);
-	}
 
+
+	}
 	@Bean
-	CommandLineRunner test() {
+	CommandLineRunner mongoTest(MongoProperties properties) {
 		return args -> {
-			System.out.println("Mongo URI = " + mongoUri);
+			System.out.println("Mongo URI = " + properties.getUri());
+			System.out.println("Mongo Database = " + properties.getDatabase());
 		};
 	}
 
-	@Bean
-	CommandLineRunner envTest() {
-		return args -> {
-			System.out.println("spring.data.mongodb.uri = " + mongoUri);
-			System.out.println("ENV MONGODB_URI = " + System.getenv("MONGODB_URI"));
-		};
-	}
 }
