f885a1a4d71cb63266d15ae462928733f9a72240
Rohit <88888rohitsharma@gmail.com>
2026-07-31 14:05:29 +0530

    add startup logs to debug MongoDB configuration

--- waterservice/src/main/java/com/fitness/waterservice/WaterserviceApplication.java
+++ waterservice/src/main/java/com/fitness/waterservice/WaterserviceApplication.java
@@ -1,13 +1,33 @@
 package com.fitness.waterservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WaterserviceApplication {
+
+	@Value("${spring.data.mongodb.uri:NOT_FOUND}")
+	private String mongoUri;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaterserviceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner test() {
+		return args -> {
+			System.out.println("Mongo URI = " + mongoUri);
+		};
+	}
+
+	@Bean
+	CommandLineRunner envTest() {
+		return args -> {
+			System.out.println("spring.data.mongodb.uri = " + mongoUri);
+			System.out.println("ENV MONGODB_URI = " + System.getenv("MONGODB_URI"));
+		};
+	}
 }
